61f588d5ce06b561ab09b95436e7b091238aacf8
pawel <ypawelborkowski@gmail.com>
2023-07-24 18:33:18 +0200

    initial commit

---
+++ src/main/java/pl/yellow/rentallo/RentalloApplication.java
@@ -0,0 +1,13 @@
+package pl.yellow.rentallo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/yellow/rentallo/RentalloApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.yellow.rentallo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
