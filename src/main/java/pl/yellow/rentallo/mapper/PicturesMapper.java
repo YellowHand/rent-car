719859786112e08f067d8f3673365c34028bc4e9
pawel <ypawelborkowski@gmail.com>
2023-07-26 18:33:09 +0200

    [add] - PicturesMapper - implemented

---
+++ src/main/java/pl/yellow/rentallo/mapper/PicturesMapper.java
@@ -0,0 +1,23 @@
+package pl.yellow.rentallo.mapper;
+
+import pl.yellow.rentallo.domain.Pictures;
+import pl.yellow.rentallo.dto.PicturesDto;
+
+public class PicturesMapper implements Mapper<Pictures, PicturesDto> {
+
+    @Override
+    public PicturesDto fromEntityToDto(Pictures entity) {
+        return PicturesDto.builder()
+                .mainPictureUrl(entity.getMainPictureUrl())
+                .picturesUrls(entity.getPicturesUrls())
+                .build();
+    }
+
+    @Override
+    public Pictures fromDtoToEntity(PicturesDto dto) {
+        return Pictures.builder()
+                .mainPictureUrl(dto.mainPictureUrl())
+                .picturesUrls(dto.picturesUrls())
+                .build();
+    }
+}
