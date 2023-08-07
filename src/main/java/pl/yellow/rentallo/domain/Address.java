5bce37a774b47914a3663407f38857b103dc13b1
pawel <ypawelborkowski@gmail.com>
2023-08-07 18:31:07 +0200

    [add] - Client and Address classes created

---
+++ src/main/java/pl/yellow/rentallo/domain/Address.java
@@ -0,0 +1,10 @@
+package pl.yellow.rentallo.domain;
+
+public class Address {
+    String street;
+    String zipCode;
+    String city;
+    String houseNumber;
+    String voivodeship;
+    String country;
+}
---
+++ src/main/java/pl/yellow/rentallo/domain/Client.java
@@ -0,0 +1,18 @@
+package pl.yellow.rentallo.domain;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+public class Client {
+    String name;
+    String surname;
+    String pesel;
+    String email;
+    LocalDate dateOfBirth;
+    String phone;
+    Address address;
+    LocalDateTime accountCreated;
+}
