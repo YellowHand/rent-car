81303faa66c37421daf2b798955cccba5793ea7d
pawel <ypawelborkowski@gmail.com>
2023-08-07 19:31:05 +0200

    [change] - translation of domain objects into entities - finished

--- src/main/java/pl/yellow/rentallo/domain/CarBooking.java
+++ src/main/java/pl/yellow/rentallo/domain/CarBooking.java
@@ -1,20 +1,32 @@
 package pl.yellow.rentallo.domain;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "RENTS")
 public class CarBooking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    @ManyToOne
     Car bookedCar;
+    @ManyToOne
     Client client;
     LocalDate startDay;
     LocalDate endDay;
     int totalPricePerDayInPolishGrosz;
+    @ManyToOne
     Address startLocation;
+    @ManyToOne
     Address endLocation;
 }
