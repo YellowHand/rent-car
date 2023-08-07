eaca15b17fcfdad9c87a3128904089017b2b312f
pawel <ypawelborkowski@gmail.com>
2023-08-07 20:54:22 +0200

    [add] - ClientDto created

--- backend-service/src/main/java/pl/yellow/rentallo/dto/ClientDto.java
+++ backend-service/src/main/java/pl/yellow/rentallo/dto/ClientDto.java
@@ -1,5 +1,19 @@
 package pl.yellow.rentallo.dto;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 //TODO
-public record ClientDto() {
+public record ClientDto(
+        Long id,
+        String name,
+        String surname,
+        String pesel,
+        String email,
+        LocalDate dateOfBirth,
+        String phone,
+        AddressDto addressDto,
+        LocalDateTime accountCreated
+) {
+
 }
