f6dcf0c3df38184599da85328190fc90dafd79b5
pawel <ypawelborkowski@gmail.com>
2023-08-07 20:48:30 +0200

    [add] - CarBookingRequestDto created

---
+++ backend-service/src/main/java/pl/yellow/rentallo/dto/CarBookingRequestDto.java
@@ -0,0 +1,15 @@
+package pl.yellow.rentallo.dto;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+public record CarBookingRequestDto(
+        Long bookedCarId,
+        Long clientId,
+        LocalDate startDate,
+        LocalDate endDate,
+        Long startLocationId,
+        Long endLocationId
+) {
+}
