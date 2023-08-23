20d5c097d8624cbd111216c2e946eeae851c72fb
pawel <ypawelborkowski@gmail.com>
2023-08-23 18:34:27 +0200

    [add] - Client component completed

--- frontend/src/app/components/clients/clients.component.ts
+++ frontend/src/app/components/clients/clients.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Client} from "../../models/client";
+import {ClientService} from "../../services/client/client.service";
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.css']
 })
-export class ClientsComponent {
+export class ClientsComponent implements OnInit {
+
+  clients: Array<Client> = []
+
+  constructor(private clientService: ClientService) {
+  }
+
+ngOnInit(): void {
+        this.clientService
+          .getClients()
+          .subscribe(clientFromServer => {
+            console.log("Data from server" + JSON.stringify(clientFromServer, null, 2))
+            this.clients = clientFromServer
+          })
+    }
 
 }
